b882f5388c20c02137bfd598a7cf383414d11b18
heybhagwan <156886461+heybhagwan@users.noreply.github.com>
2026-01-07 01:01:05 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.efbeb212ccad1ad0aaea63affc37e9eb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest);
+
