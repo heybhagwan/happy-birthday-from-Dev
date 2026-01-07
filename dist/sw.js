bde6f56a67fab76d0964ec052fbaf99c80c3adf6
heybhagwan <devc5652@gmail.com>
2026-01-07 19:17:41 +0530

    Add dist folder for deployment

---
+++ dist/sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.a4f6ec2ea87bca4baa0e11542f196650.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest);
+
