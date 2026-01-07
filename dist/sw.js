3c3e811f7b56bbee285aa059f0c71a7eb7b233da
heybhagwan <devc5652@gmail.com>
2026-01-07 19:33:22 +0530

    Fix publicPath for Github pages

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.a4f6ec2ea87bca4baa0e11542f196650.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/happy-birthday-from-Dev/precache-manifest.2b086d8cf9fb1e5073cc8d028a2bd0d5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(__precacheManifest);
 
