fa9e6cf8a90ad25cf361863d5f92bd48e8396678
heybhagwan <devc5652@gmail.com>
2026-01-07 19:48:22 +0530

    Move dist contents to root for github pages

--- dist/sw.js
+++
@@ -1,4 +0,0 @@
-importScripts("/happy-birthday-from-Dev/precache-manifest.2b086d8cf9fb1e5073cc8d028a2bd0d5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(__precacheManifest);
-
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.efbeb212ccad1ad0aaea63affc37e9eb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/happy-birthday-from-Dev/precache-manifest.2b086d8cf9fb1e5073cc8d028a2bd0d5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(__precacheManifest);
 
